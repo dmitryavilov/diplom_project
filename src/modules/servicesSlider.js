bf0fa8ea3196525b3467320d643d711414c02aa7
Dmitry Avilov <d.av89@yandex.ru>
2021-03-24 19:11:16 +0300

    compete desktop version of slider

--- src/modules/servicesSlider.js
+++ src/modules/servicesSlider.js
@@ -1,52 +1,111 @@
 const servicesSlider = () => {
     const slider = document.querySelector('.services-slider'),
+          sliderWrapper = document.getElementById('services-slider-wrapper'),
+          wrapper = document.getElementById('serv-slider-wrapper'),
           slides = slider.querySelectorAll('.slide'),
-          prevBtn = document.getElementById('serv-prev'),
+          yellow = '#ffd11a';
+
+    let sliderTransform = 0,
+        baseSlideWidth = 210,
+        slideCounter = 0;
+
+    const changeStyles = () => {
+        sliderWrapper.style.cssText = `
+            overflow: hidden;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            width: 1110px;
+        `;
+
+        wrapper.style.cssText = `
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            position: relative;
+        `;
+
+        slider.style.cssText = `
+            padding: 0;
+            transition: 0.4s;
+        `;
+
+        slides.forEach(item => {
+            item.style.minWidth = `${baseSlideWidth}px`;
+        });
+    };
+
+    const createArrows = () => {
+        const arrowStyles = `
+            width: 50px;
+            height: 50px;
+            border-radius: 50%;
+            background-color: ${yellow};
+            position: absolute;
+            top: 50%;
+            transform: translateY(-50%);
+            font-size: 25px;
+            border: none;
+        `
+
+        const arrowPrev = document.createElement('button'),
+              arrowNext = document.createElement('button');
+
+        arrowPrev.style.cssText = arrowStyles + 'left: -50px';
+        arrowNext.style.cssText = arrowStyles + 'right: -50px';
+
+        arrowPrev.id = 'serv-prev';
+        arrowNext.id = 'serv-next';
+
+        arrowPrev.textContent = '<';
+        arrowNext.textContent = '>';
+
+        wrapper.insertAdjacentElement('afterbegin', arrowPrev);
+        wrapper.insertAdjacentElement('afterbegin', arrowNext);
+        
+    };
+
+    createArrows();
+
+    const prevBtn = document.getElementById('serv-prev'),
           nextBtn = document.getElementById('serv-next');
 
-    let slideCounter = 0;
-
-    const activeSlides = () => {
-        slides.forEach(item => {
-            item.style.display = 'none'
-        });
-
-        const comeSlides = () => {
-            slides[slideCounter].style.display = 'block';
-            slides[slideCounter + 1].style.display = 'block';
-            slides[slideCounter + 2].style.display = 'block';
-            slides[slideCounter + 3].style.display = 'block';
-            slides[slideCounter + 4].style.display = 'block';
-        };
-
-        if (slideCounter + 4 !== slides.length && slideCounter !== -1) {
-            comeSlides();
-        } else if (slideCounter + 4 === slides.length) {
-            slideCounter = 0;
-            comeSlides();
-        } else {
-            slideCounter = slides.length - 5;
-            comeSlides();
-        }
-
-    };
-
-    const turnPages = e => {
-        let target = e.target;
-
-        if (target.closest('#serv-next')) {
+    const toNextSlide = () => {
+        if (slideCounter + 5 !== slides.length) {
+            sliderTransform += baseSlideWidth + 12;
+            slider.style.transform = `translateX(${-sliderTransform}px)`;
             slideCounter++;
-            activeSlides();
-        } else {
-            slideCounter--;
-            activeSlides();
         };
     };
 
-    prevBtn.addEventListener('click', turnPages);
-    nextBtn.addEventListener('click', turnPages);
+    const toPrevSlide = () => {
+        if (sliderTransform > 0) {
+            sliderTransform -= baseSlideWidth + 12;
+            slider.style.transform = `translateX(${-sliderTransform}px)`;
+            slideCounter--;
+        };
+    };
 
-    activeSlides();
+    changeStyles();
+
+    nextBtn.addEventListener('click', toNextSlide);
+    prevBtn.addEventListener('click', toPrevSlide);
+
+    wrapper.addEventListener('mouseenter', () => {
+        const checkKeyNumber = e => {
+            if (e.keyCode === 37) {
+                toPrevSlide();
+            } else if (e.keyCode === 39) {
+                toNextSlide();
+            }
+        };
+
+        document.addEventListener('keydown', checkKeyNumber);
+
+        wrapper.addEventListener('mouseleave', () => {
+            document.removeEventListener('keydown', checkKeyNumber);
+        });
+    });
 };
 
 export default servicesSlider;
